9028558dcbfb647bfdc585cfa51b675901c37a87
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-09-24 01:46:39 +0900

    生成した動画のサイズを低減する `Mp4Crf(26|32|38)` を追加しました。

---
+++ Mp4Crf32.bat
@@ -0,0 +1,16 @@
+@echo off
+if "%~1" == "" (
+	echo [ERROR] Drag and drop the mp4 file.
+	pause & exit /b 1
+)
+
+pushd %~dp0animatediff-cli-prompt-travel
+
+set SRC_PATH="%~f1"
+set DEST_PATH="%~dpn1crf32.mp4"
+
+echo ffmpeg.exe -y -i %SRC_PATH% -pix_fmt yuv420p -vcodec libx264 -tune animation -crf 32 %DEST_PATH%
+ffmpeg.exe -y -i %SRC_PATH% -pix_fmt yuv420p -vcodec libx264 -tune animation -crf 32 %DEST_PATH%
+
+popd rem %~dp0animatediff-cli-prompt-travel
+
