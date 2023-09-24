6de2da5812f59b4bf1194c0f34fc8d4a06a67196
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-09-24 14:03:34 +0900

    Mp4Crf bug fix

--- Mp4Crf.bat
+++ Mp4Crf.bat
@@ -4,15 +4,15 @@ if "%~1" == "" (
 	pause & exit /b 1
 )
 
-set SRC_PATH="%~f1"
-set DEST_PATH="%~dpn1-crf%CRF%.mp4"
-
-pushd %~dp0animatediff-cli-prompt-travel
-
 set CRF=20
 if not "%~2" == "" if not "%~2" == "0" (
 	set CRF=%~2
 )
+
+set SRC_PATH="%~f1"
+set DEST_PATH="%~dpn1-crf%CRF%.mp4"
+
+pushd %~dp0animatediff-cli-prompt-travel
 
 
 echo ffmpeg.exe -y -i %SRC_PATH% -pix_fmt yuv420p -vcodec libx264 -tune animation -crf %CRF% %DEST_PATH%
