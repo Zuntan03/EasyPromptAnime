7851146f42f1b3d361438a09d4434b97e77c1ae2
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-10-06 08:39:08 +0900

    torchとxformersのバージョン指定

--- sample/Oiyami.bat
+++ sample/Oiyami.bat
@@ -1,4 +1,4 @@
 @echo off
 call "%~dp0..\Generate.bat" "%~dp0Oiyami-L60-C16-W512-H384-T768.json"
-echo https://www.google.com/search?q=%E7%BE%8E%E5%91%B3%E3%81%97%E3%81%84%E3%83%A4%E3%83%9F%E3%83%BC%E6%84%9F%E8%AC%9D%E6%84%9F%E8%AC%9D
+echo Oiyami: https://www.google.com/search?q=%%E7%%BE%%8E%%E5%%91%%B3%%E3%%81%%97%%E3%%81%%84%%E3%%83%%A4%%E3%%83%%9F%%E3%%83%%BC%%E6%%84%%9F%%E8%%AC%%9D%%E6%%84%%9F%%E8%%AC%%9D
 pause
