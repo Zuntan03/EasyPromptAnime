e5d9a2cff1435c9267b1f0424c1ca45e65a3c6d6
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-10-08 17:58:31 +0900

    venv再構築対応

--- src/Setup.bat
+++ src/Setup.bat
@@ -22,28 +22,27 @@ if not exist venv (
 if not exist data\lora ( mkdir data\lora )
 if not exist data\vae ( mkdir data\vae )
 
-if not exist convert_vae_pt_to_diffusers.py (
-	curl -LO https://github.com/huggingface/diffusers/raw/main/scripts/convert_vae_pt_to_diffusers.py
-)
-if %errorlevel% neq 0 ( pause & popd & exit /b %errorlevel% )
-
 pushd data\models\sd
-if not exist nadenadesitai_v10.safetensors (
-	curl -Lo nadenadesitai_v10.safetensors https://civitai.com/api/download/models/84669
+if not exist xxmix9realistic_v40.safetensors (
+	curl -Lo xxmix9realistic_v40.safetensors https://civitai.com/api/download/models/102222
 	timeout /t 1 /nobreak >nul
 )
 if not exist featurelessMix_v2020237Clearvae.safetensors (
 	curl -Lo featurelessMix_v2020237Clearvae.safetensors https://civitai.com/api/download/models/168615
 	timeout /t 1 /nobreak >nul
 )
-if not exist onigiriMix_v10Clearvae.safetensors (
-	curl -Lo onigiriMix_v10Clearvae.safetensors https://civitai.com/api/download/models/168600
+if not exist featurelessCuteMix_v10Clearvae.safetensors (
+	curl -Lo featurelessCuteMix_v10Clearvae.safetensors https://civitai.com/api/download/models/168584
 	timeout /t 1 /nobreak >nul
 )
-if not exist xxmix9realistic_v40.safetensors (
-	curl -Lo xxmix9realistic_v40.safetensors https://civitai.com/api/download/models/102222
-	timeout /t 1 /nobreak >nul
-)
+@REM if not exist onigiriMix_v10Clearvae.safetensors (
+@REM 	curl -Lo onigiriMix_v10Clearvae.safetensors https://civitai.com/api/download/models/168600
+@REM 	timeout /t 1 /nobreak >nul
+@REM )
+@REM if not exist nadenadesitai_v10.safetensors (
+@REM 	curl -Lo nadenadesitai_v10.safetensors https://civitai.com/api/download/models/84669
+@REM 	timeout /t 1 /nobreak >nul
+@REM )
 for %%f in (*.safetensors) do (
 	if %%~zf LSS 10240 if %%~zf GTR 0 ( echo [BROKEN FILE]: "%%f" & del "%%f" )
 )
@@ -54,22 +53,22 @@ if not exist mm_sd_v15_v2.ckpt (
 	curl -LO https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15_v2.ckpt
 	timeout /t 1 /nobreak >nul
 )
-if not exist mm_sd_v15.ckpt (
-	curl -LO https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt
-	timeout /t 1 /nobreak >nul
-)
-if not exist mm_sd_v14.ckpt (
-	curl -LO https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v14.ckpt
-	timeout /t 1 /nobreak >nul
-)
-if not exist mm-Stabilized_high.pth (
-	curl -LO https://huggingface.co/manshoety/AD_Stabilized_Motion/resolve/main/mm-Stabilized_high.pth
-	timeout /t 1 /nobreak >nul
-)
-if not exist mm-Stabilized_mid.pth (
-	curl -LO https://huggingface.co/manshoety/AD_Stabilized_Motion/resolve/main/mm-Stabilized_mid.pth
-	timeout /t 1 /nobreak >nul
-)
+@REM if not exist mm_sd_v15.ckpt (
+@REM 	curl -LO https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt
+@REM 	timeout /t 1 /nobreak >nul
+@REM )
+@REM if not exist mm_sd_v14.ckpt (
+@REM 	curl -LO https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v14.ckpt
+@REM 	timeout /t 1 /nobreak >nul
+@REM )
+@REM if not exist mm-Stabilized_high.pth (
+@REM 	curl -LO https://huggingface.co/manshoety/AD_Stabilized_Motion/resolve/main/mm-Stabilized_high.pth
+@REM 	timeout /t 1 /nobreak >nul
+@REM )
+@REM if not exist mm-Stabilized_mid.pth (
+@REM 	curl -LO https://huggingface.co/manshoety/AD_Stabilized_Motion/resolve/main/mm-Stabilized_mid.pth
+@REM 	timeout /t 1 /nobreak >nul
+@REM )
 for %%f in (*.ckpt) do (
 	if %%~zf LSS 10240 if %%~zf GTR 0 ( echo [BROKEN FILE]: "%%f" & del "%%f" )
 )
@@ -96,7 +95,9 @@ popd rem animatediff-cli-prompt-travel\data
 
 if not exist ECCV2022-RIFE (
 	git clone https://github.com/megvii-research/ECCV2022-RIFE
-	pushd ECCV2022-RIFE
+)
+pushd ECCV2022-RIFE
+if not exist venv (
 	python -m venv venv
 	call venv\Scripts\activate.bat
 	python -m pip install --upgrade pip
@@ -108,8 +109,8 @@ if not exist ECCV2022-RIFE (
 	curl -Lo RifeModel.zip https://drive.google.com/uc?id=1APIzVeI-4ZZCEuIRE1m6WYfSCaOsi_7_
 	PowerShell -Version 5.1 -ExecutionPolicy Bypass Expand-Archive -Path RifeModel.zip -DestinationPath .
 	del RifeModel.zip
-	popd rem ECCV2022-RIFE
 )
+popd rem ECCV2022-RIFE
 
 set FFMPEG_DIR=ffmpeg-master-latest-win64-gpl
 if not exist %FFMPEG_DIR% (
